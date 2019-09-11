66253e8b5b58bc580b080928125177379b527971
Luca <Luca@DESKTOP-CDKMV53>
2019-09-11 23:24:39 +0200

    versione 23:24

--- Project/src/com/Project/servlet/Modifica_Film.java
+++ Project/src/com/Project/servlet/Modifica_Film.java
@@ -45,7 +45,7 @@ public class Modifica_Film extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.sendRedirect("show_films.jsp");
+		response.sendRedirect("home.jsp");
 		try {
 			response.setContentType("text/html");
 			PrintWriter out=response.getWriter();
@@ -58,22 +58,31 @@ public class Modifica_Film extends HttpServlet {
 				PreparedStatement statement;
 				
 				Film film = new Film();
+				film.setId(request.getParameter("id"));
 				film.setTitolo(request.getParameter("titolo"));
 				film.setData(request.getParameter("giorno"));
 				film.setOra_Init(request.getParameter("ora_init"));
 				film.setOra_Fine(request.getParameter("ora_fine"));
 				film.setDurata(request.getParameter("durata"));
-				film.setSala(request.getParameter("sala_cinema"));
+				film.setSala(request.getParameter("sala"));
+				
+				out.println(request.getParameter("id"));
+				out.println(request.getParameter("titolo"));
+				out.println(request.getParameter("giorno"));
+				out.println(request.getParameter("ora_init"));
+				out.println(request.getParameter("ora_fine"));
+				out.println(request.getParameter("durata"));
+				out.println(request.getParameter("sala_cinema"));
 				
 				String sql="update films set titolo=?,giorno=?,ora_init=?,ora_fine=?, durata=?,sala=? where id=? ;";
 				statement=connection.prepareStatement(sql);
-				statement.setString(1, film.getId());
-				statement.setString(2, film.getTitolo());
-				statement.setString(3, film.getData());
-				statement.setString(4, film.getOra_Init());
-				statement.setString(5, film.getOra_Fine());
-				statement.setString(6, film.getDurata());
-				statement.setString(7, film.getSala());				
+				statement.setString(7, film.getId());
+				statement.setString(1, film.getTitolo());
+				statement.setString(2, film.getData());
+				statement.setString(3, film.getOra_Init());
+				statement.setString(4, film.getOra_Fine());
+				statement.setString(5, film.getDurata());
+				statement.setString(6, film.getSala());				
 				statement.executeUpdate();
 				}
 			catch(ClassNotFoundException e)	{
